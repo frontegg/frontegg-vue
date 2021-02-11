ad34f92342df691b262de2b3b108a9c429b232f0
Valerie <v.hozbenko@procoders.tech>
2021-02-11 18:17:38 +0200

    started to create modal component

--- src/components/Team/TeamLayout.vue
+++ src/components/Team/TeamLayout.vue
@@ -5,11 +5,45 @@
       @onOpenModal="onOpenModal"
     />
     <TeamTable />
-    <TeamPagination 
-      :current-page="currentPage" 
-      :total-pages="totalPages" 
-      @changePage="changePage"/>
-      <TeamInviteModal :open-modal="openModal" @onCloseModal="onCloseModal"/>
+    <TeamPagination
+      :current-page="currentPage"
+      :total-pages="totalPages"
+      @changePage="changePage"
+    />
+    <FModal
+      :open-modal="openModal"
+      :btnLeftText="'Cancel'"
+      :btnRightText="'Invite'"
+      :headText="'Invite New Teammate'"
+      @onCloseModal="onCloseModal"
+    >
+      <template v-slot:content>
+        <v-form  class="fe-form">
+          <div class="fe-input__header">
+            <div class="fe-input__label">
+              Name
+            </div>
+          </div>
+          <div>
+            <v-text-field
+              name="name"
+              placeholder="Enter name"
+            />
+          </div>
+          <div class="fe-input__header">
+            <div class="fe-input__label">
+              Email
+            </div>
+          </div>
+          <div>
+            <v-text-field
+              name="email"
+              placeholder="Enter email"
+            />
+          </div>
+        </v-form>
+      </template>
+    </FModal>
   </div>
 </template>
 
@@ -18,37 +52,36 @@ import Vue from "vue";
 import TeamTableToolbar from "@/components/Team/TeamTableToolbar.vue";
 import TeamTable from "@/components/Team/TeamTable.vue";
 import TeamPagination from "@/components/Team/TeamPagination.vue";
-import TeamInviteModal from "@/components/Team/TeamInviteModal.vue";
+import FModal from "@/components/core/elements/Modal/FModal.vue";
 
 export default Vue.extend({
   name: "TeamLayout",
-  components: {TeamTableToolbar, TeamTable, TeamPagination, TeamInviteModal},
+  components: { TeamTableToolbar, TeamTable, TeamPagination, FModal },
   data() {
     return {
       currentPage: 2,
       totalPages: 8,
       openModal: false,
-    }
+    };
   },
   methods: {
     onSearchChange(val: string) {
-      console.log('onSearchChange:', val);
+      console.log("onSearchChange:", val);
     },
     onOpenModal() {
-      console.log('onOpenModal')
-      this.openModal = true
+      console.log("onOpenModal");
+      this.openModal = true;
     },
     onCloseModal() {
-      console.log('onCloseModal')
-      this.openModal = false
+      console.log("onCloseModal");
+      this.openModal = false;
     },
     changePage(val: number) {
-      console.log('Current page:',val)
-    }
+      console.log("Current page:", val);
+    },
   },
 });
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/components/core/elements/Modal/FModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <div data-app>
+    <v-dialog
+      v-model="openModal"
+      persistent
+      transition="dialog-top-transition"
+      max-width="600px"
+      :content-class="'fe-modal'"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline"> {{headText}} </span>
+          <v-btn icon dark @click="close">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <slot name="content"></slot>
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="close"> {{btnLeftText}} </v-btn>
+          <v-btn color="blue darken-1" text @click="close"> {{btnRightText}} </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamInviteModal",
+  props: {
+    openModal: {
+      type: Boolean,
+      default: false,
+    },
+    btnLeftText: {
+      type: String,
+      default: 'Close',
+    },
+    btnRightText: {
+      type: String,
+      default: 'Save',
+    },
+    headText: {
+      type: String,
+      default: 'Modal'
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("onCloseModal", false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" >
+  @import "./FeModal.scss";
+</style>
