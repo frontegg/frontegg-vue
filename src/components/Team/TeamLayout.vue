a811b4feb307abe62a218805aa2ff8eb88ca848c
Valerie <v.hozbenko@procoders.tech>
2021-02-11 15:55:30 +0200

    added a new table-pagination`s component and style

--- src/components/Team/TeamLayout.vue
+++ src/components/Team/TeamLayout.vue
@@ -5,6 +5,10 @@
       @onOpenModal="onOpenModal"
     />
     <TeamTable />
+    <TeamPagination 
+      :current-page="currentPage" 
+      :total-pages="totalPages" 
+      @changePage="changePage" />
   </div>
 </template>
 
@@ -12,16 +16,26 @@
 import Vue from "vue";
 import TeamTableToolbar from "@/components/Team/TeamTableToolbar.vue";
 import TeamTable from "@/components/Team/TeamTable.vue";
+import TeamPagination from "@/components/Team/TeamPagination.vue";
 
 export default Vue.extend({
   name: "TeamLayout",
-  components: {TeamTableToolbar, TeamTable},
+  components: {TeamTableToolbar, TeamTable, TeamPagination},
+  data() {
+    return {
+      currentPage: 2,
+      totalPages: 8,
+    }
+  },
   methods: {
     onSearchChange(val: string) {
       console.log('onSearchChange:', val);
     },
     onOpenModal() {
       console.log('onOpenModal')
+    },
+    changePage(val: number) {
+      console.log('Current page:',val)
     }
   },
 });
---
+++ src/components/Team/TeamPagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="table-pagination">
+    <v-pagination 
+      v-model="current" 
+      :length="total" 
+      @input="changePage"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamPagination",
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+    totalPages: {
+      type: Number,
+      default: 3,
+    },
+  },
+  data() {
+    return {
+      current: this.currentPage,
+      total: this.totalPages
+    }
+  },
+  methods: {
+    changePage(val) {
+      this.$emit('changePage', val)
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/pagination.scss";
+</style>
