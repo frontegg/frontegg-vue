4c320fc0faea793427abb7e8e5c9de8b36db0902
Valerie <v.hozbenko@procoders.tech>
2021-02-11 16:55:31 +0200

    created invite-model component

--- src/components/Team/TeamLayout.vue
+++ src/components/Team/TeamLayout.vue
@@ -8,7 +8,8 @@
     <TeamPagination 
       :current-page="currentPage" 
       :total-pages="totalPages" 
-      @changePage="changePage" />
+      @changePage="changePage"/>
+      <TeamInviteModal :open-modal="openModal" @onCloseModal="onCloseModal"/>
   </div>
 </template>
 
@@ -17,14 +18,16 @@ import Vue from "vue";
 import TeamTableToolbar from "@/components/Team/TeamTableToolbar.vue";
 import TeamTable from "@/components/Team/TeamTable.vue";
 import TeamPagination from "@/components/Team/TeamPagination.vue";
+import TeamInviteModal from "@/components/Team/TeamInviteModal.vue";
 
 export default Vue.extend({
   name: "TeamLayout",
-  components: {TeamTableToolbar, TeamTable, TeamPagination},
+  components: {TeamTableToolbar, TeamTable, TeamPagination, TeamInviteModal},
   data() {
     return {
       currentPage: 2,
       totalPages: 8,
+      openModal: false,
     }
   },
   methods: {
@@ -33,6 +36,11 @@ export default Vue.extend({
     },
     onOpenModal() {
       console.log('onOpenModal')
+      this.openModal = true
+    },
+    onCloseModal() {
+      console.log('onCloseModal')
+      this.openModal = false
     },
     changePage(val: number) {
       console.log('Current page:',val)
