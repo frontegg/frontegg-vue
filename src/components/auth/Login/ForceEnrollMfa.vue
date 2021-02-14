988b207bb7636d03423cc6886fdd0f9c57aff38f
Yousef Mahmoud <ymahmood@itgsoftware.com>
2021-02-14 09:43:41 +0200

    login page - SSO components

---
+++ src/components/auth/Login/ForceEnrollMfa.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+  <v-row>
+    <v-col cols="12">
+
+      <div class="fe-relative fe-mt-4">
+        <spinner />
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Spinner from "@/components/Common/Spinner.vue";
+
+export default Vue.extend({
+  name: "ForceEnrollMfa",
+  components: {
+    Spinner,
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
