a118474763a18285d8aa62ed79d46de5f10fe522
Yousef Mahmoud <ymahmood@itgsoftware.com>
2021-02-15 19:10:23 +0200

    MFA Disable components

---
+++ src/components/auth/MFA/MFADisableDialog/MFADisableDialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <MFADisableDialogMessage />
+      <MFADisableDialogForm />
+      <MFADisableDialogErrorMessage />
+      <MFADisableDialogFooter />
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import MFADisableDialogMessage from './MFADisableDialogMessage.vue';
+import MFADisableDialogFooter  from './MFADisableDialogFooter.vue';
+import MFADisableDialogForm from './MFADisableDialogForm.vue';
+import MFADisableDialogErrorMessage from './MFADisableDialogErrorMessage.vue';
+import { AuthState } from '@/plugins/fronteggAuth/Api';
+import { mapState } from '@/plugins/fronteggCore/map-state'
+
+export default Vue.extend({
+  name: "MFADisableDialog",
+  components: {
+    MFADisableDialogMessage,
+    MFADisableDialogFooter,
+    MFADisableDialogForm,
+    MFADisableDialogErrorMessage,
+  },
+  data() {
+    return {
+      ...mapState(this, {
+        mfaState: (state: { auth: AuthState }) => state.auth.mfaState,
+      }),
+    }
+  },
+  computed: {
+
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/auth/MFA/MFADisableDialog/MFADisableDialogMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { AuthState } from '@/plugins/fronteggAuth/Api';
+import { mapState } from '@/plugins/fronteggCore/map-state'
+
+export default Vue.extend({
+  name: "MFADisableDialogMessage",
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
