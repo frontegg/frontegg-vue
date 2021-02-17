b8ee227228b78b73f4c313b5ee5004cc955111cd
Valerie <v.hozbenko@procoders.tech>
2021-02-17 17:03:58 +0200

    added router`s list on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="home">
-    <img
-      alt="Vue logo"
-      src="@/assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="@/assets/logo.png" style="margin: 0 auto" class="d-block" />
+    <v-card class="mx-auto" max-width="300" tile>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item :to="'/team_management'">
+            <v-list-item-content>
+              <v-list-item-title>Team management</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item :to="'/account/login'">
+            <v-list-item-content>
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item :to="'/account/forget-password'">
+            <v-list-item-content>
+              <v-list-item-title>Forget password</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
   </div>
 </template>
-
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
+  data() {
+    return {
+      selectedItem: 1
+    };
+  }
 });
 </script>
