815bba3a1a4d6ac920c66bc155a3cbfe1921ef9f
Rotem <rotem@frontegg.com>
2022-11-10 19:46:07 +0200

    FR-9633 - authorized content guard support in Vue

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -128,6 +128,24 @@ const Frontegg: PluginObject<PluginOptions> | any = {
       }
     }, 10);
 
+
+    const isAuthRoutes = (path: string) => {
+      return Object.values(Vue.fronteggAuth.routes)
+        .filter(path => path != Vue.fronteggAuth.routes.authenticatedUrl)
+        .includes(path);
+    }
+
+    const authorizedContentGuard = (_this: any)=>{
+      if (_this.authorizedContent && !isAuthRoutes(_this.$route.path)) {
+        if (options?.hostedLoginBox) {
+          Vue.fronteggAuth.loginActions.requestHostedLoginAuthorize();
+        } else {
+          const { loginUrl } = Vue.fronteggAuth.routes
+          _this.$router.push(loginUrl);
+        }
+      }
+    }
+
     if (!pluginRegistered) {
       registerPlugins(Vue);
     }
@@ -139,6 +157,12 @@ const Frontegg: PluginObject<PluginOptions> | any = {
         setStoreKey(this, store);
         this.fronteggAuth = Vue.fronteggAuth;
         connectMapState(this);
+      },
+      updated() {
+        authorizedContentGuard(this)
+      },
+      mounted() {
+        authorizedContentGuard(this)
       },
       created() {
         if (getStoreBinding(this)) {
